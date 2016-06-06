08845dd8df0bff4ca8a0d682ad13b69ccafdb02e
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-06 17:42:23 -0400

    Update Firefox.js

--- Firefox.js
+++ Firefox.js
@@ -2,3 +2,6 @@ var w = window.open("https://www.kiwiirc.com");
 //Some wait time
 w.document.getElementsByClassName("btn")[2].click();
 //Some wait time
+var NICK = "Zachary";
+w.document.getElementById("server_select_nick").value = NICK;
+//Some wait time
