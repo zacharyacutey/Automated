c193467d1c725bafca5f69860a3e858562946e4e
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-06 17:00:00 -0400

    Update Firefox.js

--- Firefox.js
+++ Firefox.js
@@ -1,2 +1,2 @@
 var w = window.open("https://www.github.com");
-w.document.getElementsByClassName("mini-repo-list-item")[0].click();
+w,window.setTimeOut(w.document.getElementsByClassName("mini-repo-list-item")[0].click,5000);
