bda3f9e4abe3304a045e267f4670a7399eaf79f5
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-06 17:58:15 -0400

    Update Firefox.js

--- Firefox.js
+++ Firefox.js
@@ -1,18 +1,29 @@
+var NOT_OVER = true;
+function wait()
+{
+  window.setTimeout(function(){NOT_OVER=false;},5000);
+  while(NOT_OVER)
+  {
+    
+  }
+  NOT_OVER = true;
+}
+
 var NICK = "Zachary";
 var CHANNEL = "#chat";
 var SERVER = ""; //Not showwing the server
 
 var w = window.open("https://www.kiwiirc.com");
-//Some wait time
+wait();
 w.document.getElementsByClassName("btn")[2].click();
-//Some wait time
+wait();
 
 w.document.getElementById("server_select_nick").value = NICK;
-//Some wait time
+wait();
 w.document.getElementById("server_select_channel").value = CHANNEL;
-//Some wait time
+wait();
 w.document.getElementsByClassName("show_more")[0].click();
-//Some wait time
+wait();
 w.document.getElementById("server_select_server").value = SERVER;
-//Some wait time
+wait();
 w.document.getElementsByTagName("button")[0].click();
