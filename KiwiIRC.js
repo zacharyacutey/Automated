835ffc23814be4ff3493614b062675c3bb1ba8eb
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-06 21:47:42 -0400

    Update KiwiIRC.js

--- KiwiIRC.js
+++ KiwiIRC.js
@@ -1,15 +1,21 @@
 function wait(fn)
 {
-  window.setTimeout(fn,3000);
+  window.setTimeout(fn,5000);
 }
 function wait2(fn)
 {
-  window.setTimeout(fn,1);
+  window.setTimeout(fn,100);
+}
+function g(){
+w.document.getElementsByTagName("textarea")[0].value=MESSAGE;
+w.$(w.document.getElementsByTagName("textarea")[0]).trigger({type: 'keydown', which: 13, keyCode: 13});
+wait2(g);// For spamming
 }
 var NICK="SPAM";
 var CHANNEL="#chat";
 var SERVER="connorb.xyz";
 var MESSAGE="SPAM";
+var g;
 var w = window.open("https://www.kiwiirc.com");
 wait(function(){
 w.document.getElementsByClassName("btn")[2].click();
@@ -24,11 +30,7 @@ wait(function(){
 w.document.getElementById("server_select_server").value = SERVER;
 wait(function(){
 w.document.getElementsByTagName("button")[0].click();
-wait(g=function(){
-w.document.getElementsByTagName("textarea")[0].value=MESSAGE;
-w.$(w.document.getElementsByTagName("textarea")).trigger({type: 'keydown', which: 13, keyCode: 13});
-wait2(g);// For spamming
-});
+wait(g);
 });
 });
 });
