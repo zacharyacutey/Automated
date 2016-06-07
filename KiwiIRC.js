d4177d4593e4ed68c3b808c34c548b7cefb4c269
Garrett Smith <19smitgr@users.noreply.github.com>
2016-06-07 10:54:26 -0400

    Format for readability

--- KiwiIRC.js
+++ KiwiIRC.js
@@ -1,46 +1,57 @@
-function wait(fn)
-{
+function wait(fn) {
   window.setTimeout(fn,5000);
 }
-function wait2(fn)
-{
-  w.window.setTimeout(fn,500);
-}
-var NICK="SPAM";
-var CHANNEL="#chat";
-var SERVER="";
-var MESSAGE="SPAM";
-var ITERATIONS = 100;
-var ITERATED = 0;
-function g(){
-if(ITERATIONS == ITERATED)
-{
-  return;
-}
-ITERATED += 1;
-w.document.getElementsByTagName("textarea")[0].value=MESSAGE;
-w.$(w.document.getElementsByTagName("textarea")[0]).trigger({type: 'keydown', which: 13, keyCode: 13});
-wait2(g);
+
+function wait2(fn) {
+  window.setTimeout(fn,500);
 }
 
+function g() {
+	try 
+	{
+	w.document.getElementsByTagName("textarea")[0].value=MESSAGE;
+
+	w.$(w.document.getElementsByTagName("textarea")[0]).trigger({type: 'keydown', which: 13, keyCode: 13});
+
+	wait2(g);
+
+	} 
+
+	catch(e) 
+	{
+	  console.log("ERROR HAS OCCURRED");
+	}
+}
+
+var NICK = "SPAM";
+var CHANNEL = "#chat";
+var SERVER = "connorb.xyz";
+var MESSAGE = "SPAM";
 var w = window.open("https://www.kiwiirc.com");
-wait(function(){
-w.document.getElementsByClassName("btn")[2].click();
-wait(function(){
 
-w.document.getElementById("server_select_nick").value = NICK;
+
 wait(function(){
-w.document.getElementById("server_select_channel").value = CHANNEL;
-wait(function(){
-w.document.getElementsByClassName("show_more")[0].click();
-wait(function(){
-w.document.getElementById("server_select_server").value = SERVER;
-wait(function(){
-w.document.getElementsByTagName("button")[0].click();
-wait(g);
-});
-});
-});
-});
-});
+	w.document.getElementsByClassName("btn")[2].click();
+	wait(function(){
+
+		w.document.getElementById("server_select_nick").value = NICK;
+		wait2(function(){
+
+			w.document.getElementById("server_select_channel").value = CHANNEL;
+			wait2(function(){
+
+				w.document.getElementsByClassName("show_more")[0].click();
+				wait2(function(){
+
+					w.document.getElementById("server_select_server").value = SERVER;
+					wait(function(){
+
+						w.document.getElementsByTagName("button")[0].click();
+						wait(g);
+
+					});
+				});
+			});
+		});
+	});
 });
